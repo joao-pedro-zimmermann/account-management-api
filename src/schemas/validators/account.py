c38f0a0a50e04ccd38c5da9049742a7d8901d55e
joao-pedro-zimmermann <joao.pedro@startcarreiras.com>
2024-07-04 18:50:10 -0300

    feat: add account validators

---
+++ src/schemas/validators/account.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, Field
+
+class Account(BaseModel):
+    account_number: str | None
+
+class Balance(BaseModel):
+    balance: str | None
+
+class Deposit(BaseModel):
+    amount: int | None
+
+class Transfer(BaseModel):
+    amount: int | None
+    from_: str | None = Field(alias="from")
+    to: str | None
